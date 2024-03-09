0f64645890e9eee53ce094b14ccc71c44e647741
Your Name <you@example.com>
2024-03-09 01:17:54 +0100

    file not found

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # service mariadb start
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 mysqld_safe &
-sleep 10
+sleep 2
 mariadb -u root << MY_SCRIPT
-CREATE DATABASE IF NOT EXISTS wordpressdb;
-CREATE USER 'abelfany'@'%' IDENTIFIED BY '733142';
-GRANT ALL PRIVILEGES ON wordpressdb.* TO 'abelfany'@'%';
+CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
+CREATE USER '$DATABASE_USER'@'%' IDENTIFIED BY '$DATABASE_PASS';
+GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USER'@'%';
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '733142';
 shutdown 
 MY_SCRIPT
-mysqld -u root
+# mysqld -u root
+mariadbd
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+sed -i "s#listen = /run/php/php7.4-fpm.sock#listen = wordpress:9000#g" /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir -p /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/
+
+# cd /var/www/html/wordpress
+cd /var/www/html/wordpress
+wp core download --allow-root
+wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER --dbpass=$DATABASE_PASS --dbhost=$DATABASE_HOST --allow-root
+wp core install --url=http://abelfany.42.fr --title="Inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=abdalltifalfani309@gmail.com --allow-root
+wp user create jack jack@gmail.com --role=editor --user_pass=$WP_SUSER_PASS --allow-root
+chmod -R 755 /var/www/html/ 
+# touch wp-config.php
+# creat the admin, this will also creat the wp-config.php
+# cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+service php7.4-fpm start
+service php7.4-fpm stop
+
+# php-fpm7.4 -F
+exec "$@"
